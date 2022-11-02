c03f05f0078c01ba9631d786a4c74a9853040f4f
Maya084 <37115588+Maya084@users.noreply.github.com>
2022-11-02 13:06:36 +0100

    📝 Docs(notification): add explanation to functions

--- src/lib/CreateAlarm.svelte
+++ src/lib/CreateAlarm.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
+  // const time=72000000;
+  const time=3000;
   let btn: HTMLButtonElement;
 
   function createAlarm(): void {
+    //ask for permission
     Notification.requestPermission().then((perm) => {
       if (perm === "granted") {
+        //if permission is granted create a notification and then start a timer
         createNotif();
         startNotifications();
+        //disable button so user can't create many intervals
         btn.disabled = true;
       }
     });
   }
 
+  //starts interval every 20 minutes
   function startNotifications(): void {
     setInterval(() => {
+      //create notification when timer runs out
       createNotif();
-    }, 120000);
+    }, time);
   }
 
+  //creates a new push notification
   const createNotif = () => {
     new Notification("Look away!", {
       body: "It's time to look away from the screen for 20 seconds",
       vibrate: [200, 100, 200],
+      
     });
   };
+
 </script>
 
-<button on:click={createAlarm} bind:this={btn}>Create alarm</button>
+<button on:click={createAlarm} bind:this={btn}>Create a reminder</button>
