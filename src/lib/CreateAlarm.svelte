977f5f89197012252fcbf13e12177f9f39454935
Maya084 <37115588+Maya084@users.noreply.github.com>
2022-11-02 13:31:52 +0100

    ♻️  Refactoring(notification): improve notification logic

--- src/lib/CreateAlarm.svelte
+++ src/lib/CreateAlarm.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-  // const time=72000000;
-  const time=3000;
+  import { onDestroy, onMount } from "svelte";
+
+  const time = 72000000;
+  // const time=10000;
   let btn: HTMLButtonElement;
+  let interval;
 
   function createAlarm(): void {
     //ask for permission
     Notification.requestPermission().then((perm) => {
       if (perm === "granted") {
         //if permission is granted create a notification and then start a timer
-        createNotif();
         startNotifications();
-        //disable button so user can't create many intervals
-        btn.disabled = true;
       }
     });
   }
 
   //starts interval every 20 minutes
   function startNotifications(): void {
-    setInterval(() => {
+    createNotif();
+    //disable button so user can't create many intervals
+    btn.disabled = true;
+    interval = setInterval(() => {
       //create notification when timer runs out
       createNotif();
     }, time);
@@ -29,10 +32,18 @@
     new Notification("Look away!", {
       body: "It's time to look away from the screen for 20 seconds",
       vibrate: [200, 100, 200],
-      
     });
   };
 
+  onMount(() => {
+    if (Notification.permission === "granted") {
+      startNotifications();
+    }
+  });
+
+  onDestroy(() => {
+    interval = null;
+  });
 </script>
 
 <button on:click={createAlarm} bind:this={btn}>Create a reminder</button>
