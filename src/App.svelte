cbdecacda34f220b53bc7125002bc68bd0c2cc46
Maya084 <37115588+Maya084@users.noreply.github.com>
2022-11-02 13:31:10 +0100

    🎨 Style(global): add descritpion for the app / front page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-    import CreateAlarm from "./lib/CreateAlarm.svelte";
-
+  import CreateAlarm from "./lib/CreateAlarm.svelte";
 </script>
 
 <main>
-  <CreateAlarm/>
+  <CreateAlarm />
+
+  <section>
+    <h1>What's the 20-20-20 rule?</h1>
+    <p>
+      Every <strong>20</strong> minutes spent using a screen; you
+      should try to look away at something that is <strong>20</strong> feet (6
+      meters) away from you for a total of <strong>20</strong> seconds.
+    </p>
+  </section>
 </main>
 
 <style>
